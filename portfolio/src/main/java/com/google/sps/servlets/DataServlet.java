8bb587d2efcbd2beae6be774c5b2313d895e8f0e
Andrew <amgenie364@gmail.com>
2020-06-05 04:20:30 +0000

    add Post function to DataServlet.java to implement comment feature

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,14 +26,33 @@ import javax.servlet.http.HttpServletResponse;
 public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String[] wordArray = {"Hey!", "Check", "here!"};
+    /**
     String toJson = convertToJson(wordArray);
-
     response.setContentType("application/json;");
     response.getWriter().println(toJson);
+    */
   }
-  private String convertToJson(String[] wordArray) {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String getMessage = getParameter(request, "comment", "");
+    String submissionResponse = "Submitted! Thank you " + getParameter(request, "name", "");
+
+    response.setContentType("text/html;");
+    response.getWriter().println("<h3>" + submissionResponse + "</h3>");
+    response.getWriter().println("<p> <b>message sent:</b> <i>" + getMessage + "</i></p>");
+  }
+
+  /**private String convertToJson(String[] something) {
     Gson gson = new Gson();
-    return gson.toJson(wordArray);
+    return gson.toJson(something);
+  }*/
+
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
   }
 }
